a833b3a41901185d94370de2b32e0ac36d0c043b
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-01-30 21:55:54 -0500

    changed to patch phonegap channel subscription code.

--- src/com/gwtmobile/phonegap/client/Event.java
+++ src/com/gwtmobile/phonegap/client/Event.java
@@ -19,47 +19,143 @@ package com.gwtmobile.phonegap.client;
 
 
 public class Event {
+	
+	static {
+		patch();
+	}
     
 	public native static void onDeviceReady(Callback callback) /*-{
 		//PhoneGap uses "instanceof Function". Have to use callbackproxy to get around.
-	    $doc.addEventListener("deviceready", new $wnd.callbackproxy(function() {
+	    $doc.addEventListener("deviceready", function() {
 	    	callback.@com.gwtmobile.phonegap.client.Event.Callback::onEventFired()();
-	    }).callback, false);
+	    }, false);
 	}-*/;
 	
 	// below are Android specific events.
 	
 	public native static void onPause(Callback callback) /*-{
-	    $doc.addEventListener("pause", new $wnd.callbackproxy(function() {
+	    $doc.addEventListener("pause", new function() {
 	    	callback.@com.gwtmobile.phonegap.client.Event.Callback::onEventFired()();
-	    }).callback, false);
+	    }, false);
 	}-*/;
 
 	public native static void onResume(Callback callback) /*-{
-	    $doc.addEventListener("resume", new $wnd.callbackproxy(function() {
+	    $doc.addEventListener("resume", function() {
 	    	callback.@com.gwtmobile.phonegap.client.Event.Callback::onEventFired()();
-	    }).callback, false);
+	    }, false);
 	}-*/;
 
 	public native static void onBackKeyDown(Callback callback) /*-{
-	    $doc.addEventListener("backKeyDown", new $wnd.callbackproxy(function() {
+	    $doc.addEventListener("backKeyDown", function() {
 	    	callback.@com.gwtmobile.phonegap.client.Event.Callback::onEventFired()();
-	    }).callback, false);
+	    }, false);
 	}-*/;
 
 	public native static void onMenuKeyDown(Callback callback) /*-{
-	    $doc.addEventListener("menuKeyDown", new $wnd.callbackproxy(function() {
+	    $doc.addEventListener("menuKeyDown", function() {
 	    	callback.@com.gwtmobile.phonegap.client.Event.Callback::onEventFired()();
-	    }).callback, false);
+	    }, false);
 	}-*/;
 
 	public native static void onSearchKeyDown(Callback callback) /*-{
-	    $doc.addEventListener("searchKeyDown", new $wnd.callbackproxy(function() {
+	    $doc.addEventListener("searchKeyDown", function() {
 	    	callback.@com.gwtmobile.phonegap.client.Event.Callback::onEventFired()();
-	    }).callback, false);
+	    }, false);
 	}-*/;
 
 	public interface Callback {
 		public void onEventFired();
 	}
+	
+	// The patch replaces instanceof Function with typeof 'function'
+	private native static void patch() /*-{
+		$wnd.PhoneGap.Channel.prototype.subscribe = function(f, c, g) {
+		    // need a function to call
+		    if (f == null) { return; }
+		
+		    var func = f;
+		    if (typeof c == "object" && typeof f == 'function') { func = PhoneGap.close(c, f); }
+		
+		    g = g || func.observer_guid || f.observer_guid || this.guid++;
+		    func.observer_guid = g;
+		    f.observer_guid = g;
+		    this.handlers[g] = func;
+		    return g;
+		};
+		
+		$wnd.PhoneGap.Channel.prototype.subscribeOnce = function(f, c) {
+		    var g = null;
+		    var _this = this;
+		    var m = function() {
+		        f.apply(c || null, arguments);
+		        _this.unsubscribe(g);
+		    }
+		    if (this.fired) {
+			    if (typeof c == "object" && typeof f == 'function') { f = PhoneGap.close(c, f); }
+		        f.apply(this, this.fireArgs);
+		    } else {
+		        g = this.subscribe(m);
+		    }
+		    return g;
+		};
+		
+		$wnd.PhoneGap.Channel.prototype.unsubscribe = function(g) {
+		    if (typeof g == 'function') { g = g.observer_guid; }
+		    this.handlers[g] = null;
+		    delete this.handlers[g];
+		};				
+		
+		$wnd.PhoneGap.Channel.prototype.fire = function(e) {
+		    if (this.enabled) {
+		        var fail = false;
+		        for (var item in this.handlers) {
+		            var handler = this.handlers[item];
+		            if (typeof handler == 'function') {
+		                var rv = (handler.apply(this, arguments)==false);
+		                fail = fail || rv;
+		            }
+		        }
+		        this.fired = true;
+		        this.fireArgs = arguments;
+		        return !fail;
+		    }
+		    return true;
+		};
+
+		$wnd.PhoneGap.clone = function(obj) {
+			if(!obj) { 
+				return obj;
+			}
+		
+			if(obj instanceof Array){
+				var retVal = new Array();
+				for(var i = 0; i < obj.length; ++i){
+					retVal.push(PhoneGap.clone(obj[i]));
+				}
+				return retVal;
+			}
+		
+			if (typeof obj == 'function') {
+				return obj;
+			}
+		
+			if(!(obj instanceof Object)){
+				return obj;
+			}
+		
+		    if (obj instanceof Date) {
+		        return obj;
+		    }
+		    
+			retVal = new Object();
+			for(i in obj){
+				if(!(i in retVal) || retVal[i] != obj[i]) {
+					retVal[i] = PhoneGap.clone(obj[i]);
+				}
+			}
+			return retVal;
+		};
+
+	}-*/;
+
 }
