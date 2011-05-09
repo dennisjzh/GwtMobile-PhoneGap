a4d6057abe2ce27f7501645a20e77281268bc54f
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-05-08 21:35:32 -0400

    changed to use child browser for phonegap doc page.

--- kitchensink/com/gwtmobile/phonegap/kitchensink/client/PhoneGapHeaderPanel.java
+++ kitchensink/com/gwtmobile/phonegap/kitchensink/client/PhoneGapHeaderPanel.java
@@ -2,6 +2,7 @@ package com.gwtmobile.phonegap.kitchensink.client;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.gwtmobile.phonegap.client.plugins.ChildBrowser;
 import com.gwtmobile.ui.client.utils.Utils;
 import com.gwtmobile.ui.client.widgets.HeaderPanel;
 
@@ -17,7 +18,8 @@ public class PhoneGapHeaderPanel extends HeaderPanel {
 				String caption = PhoneGapHeaderPanel.this.getCaption();
 				url = url + "phonegap_" + caption.toLowerCase() + "_" + caption.toLowerCase() + 
 					".md.html#" + caption;
-				Utils.loadUrl(url);
+				//Utils.loadUrl(url);
+				ChildBrowser.showWebPage(url, true);
 			}
 		});
 	}
