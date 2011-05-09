28e4d00c1e7aa00090ebe1cd14aa1b0143c215ff
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-05-08 21:36:23 -0400

    adding plugins to kitchensink demo.

--- kitchensink/com/gwtmobile/phonegap/kitchensink/client/PluginsUi.java
+++ kitchensink/com/gwtmobile/phonegap/kitchensink/client/PluginsUi.java
@@ -22,14 +22,14 @@ import com.google.gwt.user.client.ui.Widget;
 import com.gwtmobile.ui.client.event.SelectionChangedEvent;
 import com.gwtmobile.ui.client.page.Page;
 
-public class MainUi extends Page {
+public class PluginsUi extends Page {
 
-	private static MainUiUiBinder uiBinder = GWT.create(MainUiUiBinder.class);
+	private static PluginsUiUiBinder uiBinder = GWT.create(PluginsUiUiBinder.class);
 
-	interface MainUiUiBinder extends UiBinder<Widget, MainUi> {
+	interface PluginsUiUiBinder extends UiBinder<Widget, PluginsUi> {
 	}
 
-	public MainUi() {
+	public PluginsUi() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
@@ -37,43 +37,8 @@ public class MainUi extends Page {
 	void onListSelectionChanged(SelectionChangedEvent e) {
     	switch (e.getSelection()) {
     	case 0:
-    		goTo(new AccelerometerUi());
     		break;
     	case 1:
-    		goTo(new CameraUi());
-    		break;
-    	case 2:
-    		goTo(new CompassUi());
-    		break;
-    	case 3:
-    		goTo(new ContactsUi());
-    		break;
-    	case 4:
-    		goTo(new DeviceUi());
-    		break;
-    	case 5:
-    		goTo(new EventUi());
-    		break;
-    	case 6:
-    		goTo(new FileUi());
-    		break;
-    	case 7:
-    		goTo(new GeolocationUi());
-    		break;
-    	case 8:
-    		goTo(new MediaUi());
-    		break;
-    	case 9:
-    		goTo(new NetworkUi());
-    		break;
-    	case 10:
-    		goTo(new NotificationUi());
-    		break;
-    	case 11:
-    		goTo(new PluginsUi());
-    		break;
-    	case 12:
-    		goTo(new StorageUi());
     		break;
     	}
     }
