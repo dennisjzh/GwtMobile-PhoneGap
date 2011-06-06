aca9406bfdb1d557f01071639cbc591fb3770b0d
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-06-06 02:17:58 -0400

    moving along with File kitchen sink demo.

--- kitchensink/com/gwtmobile/phonegap/kitchensink/client/DirectoryUi.java
+++ kitchensink/com/gwtmobile/phonegap/kitchensink/client/DirectoryUi.java
@@ -41,49 +41,6 @@ public class DirectoryUi extends Page {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 	
-	@Override
-	public void onLoad() {
-		super.onLoad();
-		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-			@Override
-			public void execute() {
-				init();
-			}
-		});
-	}
-
-	private void init() {
-//		dirName = FileMgr.getRootPaths()[0] + "/gwtmobile-phonegap/";
-//		fileName = dirName + "kitchensink-file.txt";
-//		writer = FileMgr.newWriterInstance(fileName, true);
-//		reader = FileMgr.newReaderInstance();
-//
-//		Callback callback = new Callback() {			
-//			@Override
-//			public void onEvent(Event evt) {
-//				text.setHTML("Event Type: " + evt.getType() + "<br/>" + 
-//						"FileName: " + evt.getTarget().getFileName() + "<br/>" +  
-//						"Result: " + evt.getTarget().getResult() + "<br/>" +  
-//						(evt.getType().equals("error") ? ("Error: " + evt.getTarget().getError().getCode() + "<br/>") : "") +  
-//						text.getText());
-//			}
-//		};
-//		
-//		reader.onLoad(callback);
-//		reader.onLoadStart(callback);
-//		reader.onLoadEnd(callback);
-//		reader.onProgress(callback);
-//		reader.onAbort(callback);
-//		reader.onError(callback);
-//		
-//		writer.onWriteStart(callback);
-//		writer.onWriteEnd(callback);
-//		writer.onWrite(callback);
-//		writer.onProgress(callback);
-//		writer.onAbort(callback);
-//		writer.onError(callback);
-	}
-	
     @UiHandler("list0")
 	void onList0SelectionChanged(SelectionChangedEvent e) {
     	switch (e.getSelection()) {
@@ -91,6 +48,37 @@ public class DirectoryUi extends Page {
     		getDirectory();
     		break;
     	case 1:
+    		getMetadata();
+    		break;
+    	case 2:
+    		isFile();
+    		break;
+    	case 3:
+    		isDirectory();
+    		break;
+    	case 4:
+    		getDirectory();
+    		break;
+    	case 5:
+    		moveTo();
+    		break;
+    	case 6:
+    		copyTo();
+    		break;
+    	case 7:
+    		toURI();
+    		break;
+    	}
+    }
+
+    @UiHandler("list1")
+	void onList1SelectionChanged(SelectionChangedEvent e) {
+    	switch (e.getSelection()) {
+    	case 0:
+    		remove();
+    		break;
+    	case 1:
+    		getDirParent();
     		break;
     	case 2:
     		break;
@@ -99,13 +87,159 @@ public class DirectoryUi extends Page {
     	case 4:
     		break;
     	case 5:
-    		getFreeDiskSpace();
+    		break;
+    	case 6:
+    		break;
+    	case 7:
+    		break;
+    	case 8:
     		break;
     	}
     }
 
-
     private void getDirectory() {
+    	getDemoDirectory(new DemoCallback() {
+			@Override
+			public void onSuccess(DirectoryEntry dir) {
+				text.setHTML("succeed: directory full path -- " + dir.getFullPath());
+			}
+		});
+	}
+
+    private void getMetadata() {
+    	getDemoDirectory(new DemoCallback() {
+			@Override
+			public void onSuccess(DirectoryEntry dir) {
+				dir.getMetadata(new MetadataCallback() {
+					@Override
+					public void onSuccess(Metadata metadata) {
+						text.setHTML("succeed: last modification time -- " + metadata.getModificationTime().toString());
+					}
+					@Override
+					public void onError(FileError error) {
+						text.setHTML("error:" + error.getCode());
+					}
+				});
+			}
+		});
+	}
+
+    private void isFile() {
+    	getDemoDirectory(new DemoCallback() {
+			@Override
+			public void onSuccess(DirectoryEntry dir) {
+				text.setHTML("is file -- " + dir.isFile());
+			}
+		});
+	}
+
+    private void isDirectory() {
+    	getDemoDirectory(new DemoCallback() {
+			@Override
+			public void onSuccess(DirectoryEntry dir) {
+				text.setHTML("is file -- " + dir.isDirectory());
+			}
+		});
+	}
+
+    private void moveTo() {
+    	getDemoDirectory(new DemoCallback() {
+			@Override
+			public void onSuccess(final DirectoryEntry dir) {
+				dir.getParent(new EntryCallback() {
+					@Override
+					public void onSuccess(Entry entry) {
+						DirectoryEntry parent = (DirectoryEntry) entry;
+						dir.moveTo(parent, "backup-gwtmobile-phonegap", new EntryCallback() {
+							@Override
+							public void onSuccess(Entry entry) {
+								text.setHTML("succeed: moved to --" + entry.getFullPath());
+							}							
+							@Override
+							public void onError(FileError error) {
+								text.setHTML("error:" + error.getCode());
+							}
+						});
+					}
+					@Override
+					public void onError(FileError error) {
+						text.setHTML("error:" + error.getCode());
+					}
+				});
+			}
+		});
+	}
+
+    private void copyTo() {
+    	getDemoDirectory(new DemoCallback() {
+			@Override
+			public void onSuccess(final DirectoryEntry dir) {
+				dir.getParent(new EntryCallback() {
+					@Override
+					public void onSuccess(Entry entry) {
+						DirectoryEntry parent = (DirectoryEntry) entry;
+						dir.copyTo(parent, "backup-gwtmobile-phonegap", new EntryCallback() {
+							@Override
+							public void onSuccess(Entry entry) {
+								text.setHTML("succeed: copied to --" + entry.getFullPath());
+							}							
+							@Override
+							public void onError(FileError error) {
+								text.setHTML("error:" + error.getCode());
+							}
+						});
+					}
+					@Override
+					public void onError(FileError error) {
+						text.setHTML("error:" + error.getCode());
+					}
+				});
+			}
+		});
+	}
+
+    private void toURI() {
+    	getDemoDirectory(new DemoCallback() {
+			@Override
+			public void onSuccess(final DirectoryEntry dir) {
+				text.setHTML("URI --" + dir.toURI());
+			}
+		});
+	}
+
+    private void remove() {
+    	getDemoDirectory(new DemoCallback() {
+			@Override
+			public void onSuccess(final DirectoryEntry dir) {
+				dir.remove(new EventCallback() {
+					@Override
+					public void onEvent(Event evt) {
+						text.setHTML("remove --" + evt.getTarget());
+					}
+				});
+			}
+		});
+    }
+
+    private void getDirParent() {
+    	getDemoDirectory(new DemoCallback() {
+			@Override
+			public void onSuccess(final DirectoryEntry dir) {
+				dir.getParent(new EntryCallback() {
+					@Override
+					public void onSuccess(Entry entry) {
+						text.setHTML("succeed: parent --" + entry.getFullPath());
+					}
+					@Override
+					public void onError(FileError error) {
+						text.setHTML("error:" + error.getCode());
+					}
+				});
+			}
+		});
+    }
+
+    private void getDemoDirectory(final DemoCallback callback) {
     	FileMgr.requestFileSystem(LocalFileSystem.PERSISTENT, new FileSystemCallback() {
 			@Override
 			public void onSuccess(FileSystem fs) {
@@ -113,9 +247,8 @@ public class DirectoryUi extends Page {
 					@Override
 					public void onSuccess(Entry entry) {
 						DirectoryEntry dir = (DirectoryEntry) entry;
-						text.setHTML("succeed:" + dir.getFullPath());
+						callback.onSuccess(dir);
 					}
-					
 					@Override
 					public void onError(FileError error) {
 						text.setHTML("error:" + error.getCode());
@@ -129,7 +262,7 @@ public class DirectoryUi extends Page {
 		});
 	}
 
-	public void getFreeDiskSpace() {
+    public void getFreeDiskSpace() {
 		FileMgr.getFreeDiskSpace(new FreeDiskSpaceCallback() {			
 			@Override
 			public void onSuccess(double freeDiskSpace) {
@@ -142,4 +275,7 @@ public class DirectoryUi extends Page {
 		});
 	}
 
+    interface DemoCallback {
+    	void onSuccess(DirectoryEntry dir);
+    }
 }
