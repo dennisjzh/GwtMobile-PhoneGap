603ebab77b86451d7b6d2bdd05bf1df547ee7a32
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-06-06 23:51:02 -0400

    allow phonegap url link to be different than header panel caption.

--- kitchensink/com/gwtmobile/phonegap/kitchensink/client/PhoneGapHeaderPanel.java
+++ kitchensink/com/gwtmobile/phonegap/kitchensink/client/PhoneGapHeaderPanel.java
@@ -7,6 +7,8 @@ import com.gwtmobile.ui.client.widgets.HeaderPanel;
 
 public class PhoneGapHeaderPanel extends HeaderPanel {
 
+	private String _doc;
+	
 	public PhoneGapHeaderPanel() {
 		this.setLeftButton("Back");
 		this.setRightButton("Doc");
@@ -15,10 +17,24 @@ public class PhoneGapHeaderPanel extends HeaderPanel {
 			public void onClick(ClickEvent event) {
 				String url = "http://docs.phonegap.com/";
 				String caption = PhoneGapHeaderPanel.this.getCaption();
-				url = url + "phonegap_" + caption.toLowerCase() + "_" + caption.toLowerCase() + 
+				url = url + "phonegap_" + getDocument() + "_" + getDocument() + 
 					".md.html#" + caption;
 				ChildBrowser.showWebPage(url, true);
 			}
 		});
+	}
+	
+	public void setDocument(String doc) {
+		_doc = doc;
+	}
+	
+	public String getDocument() {
+		if (_doc == null) {
+			return getCaption().toLowerCase();
+		}
+		else {
+			return _doc;
+		}
+		
 	}
 }
