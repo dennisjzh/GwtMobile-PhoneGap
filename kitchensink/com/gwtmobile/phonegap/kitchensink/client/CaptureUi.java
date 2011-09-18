b875fa0315e36daa84b7d2f2c7ad2716166b287a
root <root@i930.(none)>
2011-09-18 13:33:22 -0400

    license stuff.

--- kitchensink/com/gwtmobile/phonegap/kitchensink/client/CaptureUi.java
+++ kitchensink/com/gwtmobile/phonegap/kitchensink/client/CaptureUi.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010 Zhihua (Dennis) Jiang
+/* Copyright (c) 2011 Zhihua (Dennis) Jiang
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
